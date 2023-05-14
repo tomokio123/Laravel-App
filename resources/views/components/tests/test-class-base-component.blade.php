e7d28b177ad26c8093ab80be791ba1f653cbd72e
t.fukuyama <t.fukuyama0123@gmail.com>
2023-05-14 11:41:19 +0800

    classBase

---
+++ resources/views/components/tests/test-class-base-component.blade.php
@@ -0,0 +1,7 @@
+<div>
+    クラスベースのコンポーネンとなんです
+    <div>{{ $classBaseMessage }}</div>
+    <div>{{ $defaultMessage }}</div>
+    <div>----------------------</div>
+    <!-- Let all your things have their places; let each part of your business have its time. - Benjamin Franklin -->
+</div>
--- resources/views/tests/component-test2.blade.php
+++ resources/views/tests/component-test2.blade.php
@@ -3,4 +3,17 @@
   <x-slot name="header">header2</x-slot>
     component2
     <x-tests.card title="タイトル2" content="本文2"></x-tests.card>
+    <x-test-class-base />{{--testディレクトリの中だから「test.」が要るのかと思ったが、
+      test-class-baseファイルは今のところ一意なので多分書いてはいけないのだろう --}}
+
+    <x-test-class-base classBaseMessage="test2.bladeでmessageを上書き" />
+    <div class="mb-4"></div>
+    <x-test-class-base classBaseMessage="est2.bladeでclassBaseMessageを上書き" defaultMessage="test2.bladeでdefaultMessageを上書き" />
+
+    {{-- <!-- そもそも<x-test-class-base classBaseMessage="メッ世辞です" />は、
+      「test-class-base-component.bladeファイルの$$classBaseMessageに"メッ世辞です"という値を入れ、
+      その状態の同ファイルを表示する」ってこと。また、このままでは上の「classBaseMessage」は
+      使用する箇所(<div>{{ $classBaseMessage }}</div>)では、まだ定義されていないことになるのでTestClassBaseクラスで
+    初期化メソッドを回す必要がある。それが「public function __construct($classBaseMessage){}」である-->
+    <!-- TestClassBaseクラスにも変数定義(classBaseMessageを定義)しないといけない  --> --}}
 </x-tests.app>
