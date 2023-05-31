dbec2dadfd56bb0a1bebd25b328a3102dcf58035
t.fukuyama <t.fukuyama0123@gmail.com>
2023-05-31 18:01:56 +0800

    view修正

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -17,11 +17,16 @@
                     <div class="w-1/4 p-2 md:p-4">
                       {{--productにはIDが振られているので[主キー]、そのまま$product->idでOK--}}
                       <a href=""> 
-                        <div class="border rounded-md p-4 max-h-30">
+                        <div class="border rounded-md p-4 max-h-30 bg-white-500">
                           {{--productの中にはfilenameが無いので一旦imageFirstに繋いであげる--}}
                           {{--//image1を設定してない場合はnullを入れることにしたので、ここでnull判定をする。文字列の場合は:filename->filenameと書き換える--}}
                             <x-thumbnail filename="{{ $product->imageFirst->filename ?? '' }}" type="products" />
-                            <div class="text-gray-700">{{ $product->name}}</div>
+                              <div class="mt-4">
+                                <h3 class="text-gray-600 text-xs tracking-widest title-font mb-1">{{ $product->category->name }}</h3>
+                                <h2 class="text-gray-900 title-font text-lg font-medium">{{ $product->name }}</h2>
+                                <p class="mt-1">{{ number_format($product->price) }} <span class="text-sm text-gray-700">円(税込)</span></p>
+                              </div>
+                            {{--<div class="text-gray-700">{{ $product->name}}</div>--}}
                         </div>
                       </a>
                     </div>
