4debc8dc470672bf29bed346842a1ad39a4f0dd6
t.fukuyama <t.fukuyama0123@gmail.com>
2023-06-02 11:01:23 +0800

    User/ProductnのリレーションとしてCartsを定義

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 use App\Models\Shop;
 use App\Models\SecondaryCategory;
 use App\Models\Image;
+use App\Models\User;
 use App\Models\Stock;
 
 class Product extends Model
@@ -59,5 +60,13 @@ class Product extends Model
     public function stock()
     {
         return $this->hasMany(Stock::class);
+    }
+
+    //
+    public function users()
+    {
+        //多対多(Product<=>User)の関係を定義。belongsToManyをProduct/Userの両方に書く
+        return $this->belongsToMany(User::class, 'carts')//中間テーブル="carts"とする
+        ->withPivot(['id', 'quantity']);//多対多実現のために中間テーブル(Pivot)を紐づける
     }
 }
